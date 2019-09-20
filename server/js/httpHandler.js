10a80d6baa8a0e7bd19f8274dd015cd1738d1abc
pfaithmtan <pfaithmtan@gmail.com>
2019-09-20 15:53:34 -0700

    Add test for server handling GET request
    - Add helper function to get random command
    - Include random command in the response

--- server/js/httpHandler.js
+++ server/js/httpHandler.js
@@ -12,9 +12,23 @@ module.exports.initialize = (queue) => {
   messageQueue = queue;
 };
 
+var generateRandomCommand =  function() {
+  // get random index first
+  // get the corresponding command from array of all commands
+  // return string command
+
+  var commands = ["left", "right", "up", "down"];
+  var randomIndex = Math.floor(Math.random() * commands.length);
+
+  return commands[randomIndex];
+}
+
 module.exports.router = (req, res, next = ()=>{}) => {
   console.log('Serving request type ' + req.method + ' for url ' + req.url);
   res.writeHead(200, headers);
+  if (req.method === 'GET') {
+    res.write(`SwimTeam.move("${generateRandomCommand()}")`);
+  }
   res.end();
   next(); // invoke next() at the end of a request to help with testing!
 };
